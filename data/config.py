4910981f162d644f32d5b9feafec5349a15ebf79
Ulug'bek Xatamjonov <81636710+UlugbekXatamjonov@users.noreply.github.com>
2022-11-26 14:46:39 +0500

    fix bugs

--- data/config.py
+++ data/config.py
@@ -1,7 +1,7 @@
-# from environs import Env
+from environs import Env
 
-# env = Env()
-# env.read_env()
+env = Env()
+env.read_env()
 
 # BOT_TOKEN = env.str("BOT_TOKEN")  # Bot toekn
 # ADMINS = env.list("ADMINS")  # adminlar ro'yxati
@@ -11,11 +11,9 @@
 
 import os
 
-# .env fayli ichidan quyidagilarni o'qiymiz
-BOT_TOKEN = str(os.environ.get(("BOT_TOKEN")))
-ADMINS = str(os.environ.get(("ADMINS")))
-IP = str(os.environ.get(("ip")))
-PROVIDER_TOKEN = str(os.environ.get(("PROVIDER_TOKEN")))
-
-
+# .env fayl ichidan quyidagilarni o'qiymiz
+BOT_TOKEN = str(os.environ.get("BOT_TOKEN"))  # Bot token
+ADMINS = str(os.environ.get("ADMINS"))  # adminlar ro'yxati
+IP = str(os.environ.get("ip"))  # Xosting ip manzili
+PROVIDER_TOKEN = str(os.environ.get("PROVIDER_TOKEN"))
 
